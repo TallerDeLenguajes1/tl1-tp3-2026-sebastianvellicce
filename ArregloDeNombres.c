481d7a0aaedee4adfc2ddc91d1f06ad7779e70b5
sebastianvellicce <svellicceleal@gmail.com>
2026-05-02 12:54:09 -0300

    Se realizaron el punto 1 y el punto 2

---
+++ ArregloDeNombres.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CANTIDAD_NOMBRES 5
+
+void MostrarPersonas(char *V[])
+{
+    int i;
+    printf("----------LOS NOMBRES SON------\n");
+    for(i = 0; i < CANTIDAD_NOMBRES; i++)
+    {
+        printf("%s\n", V[i]);
+    }
+}
+
+int main()
+{
+    int i;
+    char *Buff;
+    char *V[CANTIDAD_NOMBRES];
+
+    Buff = (char*)malloc(sizeof(char)*100);
+
+    for(i = 0; i<CANTIDAD_NOMBRES;i++)
+    {
+        printf("Ingrese el nombre N%d\n",i+1);
+        gets(Buff);
+        V[i]=(char*)malloc(sizeof(char)*strlen((Buff)+1));
+        strcpy(V[i],Buff);
+    }
+    MostrarPersonas(V);
+    return 0;
+}
