eefa7ebbc3ca991bf423f5ac825c488de4d96167
sebastianvellicce <svellicceleal@gmail.com>
2026-05-02 21:04:37 -0300

    Se agrego la funcion para poder mostrar al cliente y a los productos.

--- distribuidora.c
+++ distribuidora.c
@@ -21,6 +21,30 @@ float calcularCostoProducto(Producto *producto)
 {
     return(producto->PrecioUnitario*producto->Cantidad);    
 }
+void MostrarClientes(Cliente *clientes, int cantidadClientes)
+{
+    int i, j;
+    float totalCliente;
+
+    for(i = 0; i < cantidadClientes; i++)
+    {
+        totalCliente = 0;
+        printf("\n--- Cliente %d ---\n", clientes[i].ClienteID + 1);
+        printf("Nombre: %s\n", clientes[i].NombreCliente);
+        printf("Cantidad de productos: %d\n", clientes[i].CantidadProductosAPedir);
+
+        for(j = 0; j < clientes[i].CantidadProductosAPedir; j++)
+        {
+            printf("\n  Producto %d:\n", clientes[i].Productos[j].ProductoID + 1);
+            printf("  Tipo: %s\n", clientes[i].Productos[j].TipoProducto);
+            printf("  Cantidad: %d\n", clientes[i].Productos[j].Cantidad);
+            printf("  Precio unitario: %.2f\n", clientes[i].Productos[j].PrecioUnitario);
+            printf("  Costo: %.2f\n", calcularCostoProducto(&clientes[i].Productos[j]));
+            totalCliente += calcularCostoProducto(&clientes[i].Productos[j]);
+        }
+        printf("\nTotal a pagar: %.2f\n", totalCliente);
+    }
+}
 
 
 
@@ -29,6 +53,7 @@ int main()
     char *TipoProductos[] = {"Galletas","Snacks", "Cigarrillos", "Caramelos", "Bebidas"};
     char *Buff;
     int cantidadClientes,i,j,tipoRand;
+    float costo, costoFinal=0;
     Cliente *clientes;
     srand(time(NULL));
     
@@ -44,7 +69,7 @@ int main()
         printf("Ingrese el nombre de su cliente\n");
         getchar();
         gets(Buff);
-        clientes[i].NombreCliente= (char*)malloc(sizeof(char)*(Buff+1));
+        clientes[i].NombreCliente= (char*)malloc(sizeof(char) * (strlen(Buff)+1));
         strcpy(clientes[i].NombreCliente,Buff);
         clientes[i].CantidadProductosAPedir = 1 + rand()%4;
         clientes[i].Productos = (Producto*) malloc(sizeof(Producto)* clientes[i].CantidadProductosAPedir);
@@ -53,14 +78,15 @@ int main()
         {
             clientes[i].Productos[j].ProductoID = j;
             clientes[i].Productos[j].Cantidad= 1 + rand()%10;
-            tipoRand = 1 + rand()%5;
+            tipoRand =  rand()%5;
             clientes[i].Productos[j].TipoProducto = TipoProductos[tipoRand];
             clientes[i].Productos[j].PrecioUnitario = 10 + rand()%91;
-            calcularCostoProducto(&clientes[i].Productos[j]); 
+            costo = calcularCostoProducto(&clientes[i].Productos[j]);
+            costoFinal = costoFinal + costo; 
         }
     }
     
-
+    MostrarClientes(clientes, cantidadClientes);
 
 
 
